69b087561a0e628bd2da229fc7ef9a6051dcdd94
DiscSeven <linus.hedenberg@gmail.com>
2015-06-20 12:05:00 +0200

    Small fix.

--- plot2.R
+++ plot2.R
@@ -1,18 +1,17 @@
 ## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
-## PED <- readRDS("summarySCC_PM25.rds")
+PED <- readRDS("summarySCC_PM25.rds")
 
 ## We first subset the Baltimore City, Maryland (fips == "24510") values.
 PED_BCM  <- PED[PED$fips=="24510",]
 
-## We first need to sum up all Emission-values for each year.
+## We then need to sum up all Emission-values for each year.
 BCM_emissions_year <- aggregate(Emissions ~ year, PED_BCM, sum)
 
 png('plot2.png')
-## The emission per year value is divided by 100 to be easier to read
-barplot(	height=BCM_emissions_year$Emissions/100, 
+barplot(	height=BCM_emissions_year$Emissions, 
 					names.arg=BCM_emissions_year$year, 
 					xlab="years", 
-					ylab=expression('total PM2.5 emission in 100 tons'),
+					ylab=expression('total PM2.5 emission'),
 					main=expression('Total PM2.5 emissions in Baltimore City, Maryland for years'))
 dev.off()
